87e3ae91e168d85c740bf5df8283255f0de199e2
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-04-05 10:48:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,131 @@
+#! /usr/bin/env node
+import { parse } from "@babel/parser";
+import { traverse } from "@babel/core";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalk from "chalk";
+import { readFileSync, writeFile } from "fs";
+import * as ejs from "ejs";
+import path from "path";
+
+function generateTest(filepath) {
+  const code = readFileSync(filepath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    return data.toString();
+  });
+
+  let className = "";
+  let fnNames = [];
+  let spyList = [];
+  let importList = [];
+  let declareSpyList = [];
+
+  const ast = parse(code, {
+    plugins: ["typescript", "decorators", "throwExpressions"],
+    sourceType: "module",
+  });
+
+  traverse(ast, {
+    ClassDeclaration(path) {
+      if (path.node.type === "ClassDeclaration") {
+        className = path.node.id.name;
+      }
+    },
+    ClassMethod(path) {
+      if (
+        path.node.type === "ClassMethod" &&
+        path.node.kind !== "get" &&
+        path.node.kind !== "set" &&
+        path.node.kind !== "constructor"
+      ) {
+        if (!path.node.key.name.includes("ng")) {
+          fnNames.push(path.node.key.name);
+        }
+      }
+
+      if (
+        path.node.type === "ClassMethod" &&
+        path.node.kind === "constructor"
+      ) {
+        path.node.params.forEach((param) => {
+          spyList.push(
+            param.parameter.typeAnnotation.typeAnnotation.typeName.name
+          );
+        });
+        path.node.params.forEach((param) => {
+          declareSpyList.push({
+            var: param.parameter.name,
+            type: param.parameter.typeAnnotation.typeAnnotation.typeName.name,
+          });
+        });
+      }
+    },
+    ImportDeclaration(path) {
+      if (path.node.type === "ImportDeclaration") {
+        if (path.node.specifiers.length === 1) {
+          if (path.node.specifiers[0].imported.name === className) {
+            importList.push({
+              import: path.node.specifiers[0].imported.name,
+              source: path.node.source.value,
+            });
+          }
+          importList.push({
+            import: path.node.specifiers[0].local.name,
+            source: path.node.source.value.trim(),
+          });
+        }
+      }
+    },
+  });
+
+  const newImportList = importList.filter((imp) =>
+    spyList.includes(imp.import)
+  );
+
+  const template = path.join("./templates/test-component.ejs");
+  const data = {
+    fnNames: fnNames,
+    className: className,
+    importList: newImportList,
+    declareSpyList: declareSpyList,
+  };
+
+  ejs.renderFile(template, data, {}, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    if (data) {
+      writeFile(filepath.replace(".ts", ".spec.ts"), data, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(chalk.bgGreen("File written successfully"));
+      });
+    }
+  });
+}
+
+function main() {
+  console.log(gradient("orange", "yellow", "red")("Angular Test Generator"));
+  inquirer
+    .prompt([
+      {
+        name: "filepath",
+        message: "What is the absolute path to the file?",
+        type: "input",
+      },
+    ])
+    .then((answers) => {
+      generateTest(answers.filepath);
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(chalk.bgRed(err.message));
+      }
+    });
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Angular Test Generator
+
+<br>
+
+A tool to help you write unit tests in Angular. It parses the component file to scan through all functions and dependencies to inject. The CLI generated spec will be replaced by newer content that will have all `it` blocks generated for you with description already containing the name of the function. All dependencies in the constructor will be declared as spies and imported for you.
+
+## How to get started...
+
+1. Clone the repo using `git clone`
+2. Run `npm install -g` while inside the repo on your terminal.
+3. The command to run the generator is `test-gen`.
+4. Your will see a colorful prompt saying **Angular Test Generator** and asking you to add the absolute path to the file for which you want to generate the tests.
+5. If the file is found, then the tool will overwrite the existing `spec.ts` file with newer content, i.e. `it` blocks with function names, imports and declarations and a success message in green will be displayed. If anything goes wrong, an error in red will be displayed.
+
+## To uninstall
+
+Go to the repo and run `npm uninstall -g` to uninstall the tool.
